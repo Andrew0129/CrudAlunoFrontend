95644e4ff0b6778a6c645ceb68559e2540c46aad
Andrew Oliveira Benevides <andrew.benevides@w3k.com.br>
2022-11-06 16:38:12 -0300

    melhoria da api e diretorios

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,158 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">Cadastro alunos</a>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <ul>
+        <li v-for="(erro, index) of errors" :key="index">
+          campo <b>{{erro.field}}</b> - {{erro.defaultMessage}}
+        </li>
+      </ul>
+
+
+      <form @submit.prevent="salvar">
+
+          <label>Nome</label>
+          <input type="text" placeholder="Nome" v-model="student.name" >
+          <label>CPF</label>
+          <input type="text" placeholder="cpf" v-model="student.cpf" >
+          <label>Email</label>
+          <input type="email" placeholder="email" v-model="student.email" >
+
+          <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+
+      </form>
+
+      <table>
+
+        <thead>
+
+          <tr>
+            <th>RA</th>
+            <th>Name</th>
+            <th>CPF</th>
+            <th>Email</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          <tr v-for="student of students" :key="student.ra">
+            
+            <td>{{ student.ra }}</td>
+            <td>{{ student.name }}</td>
+            <td>{{ student.cpf }}</td>
+            <td>{{ student.email }}</td>
+            <td>
+              <button @click="editar(student)" class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+              <button @click="remover(student.ra)" class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+            </td>
+
+          </tr>
+
+        </tbody>
+      
+      </table>
+
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Student from './services/StudentsServices'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+      student:{
+        cpf: '',
+        name: '',
+        email:'',
+      },
+      students: [],
+      errors: []
+    }
+  },
+
+  mounted(){
+    this.listar()
+  },
+
+  methods:{
+    
+    listar(){
+      Student.listar().then(resposta => {
+        this.students = resposta.data
+      }).catch(e => {
+        console.log(e)
+      })
+    },
+
+    salvar(){
+
+      if(!this.student.ra){
+
+        Student.salvar(this.student).then(() => {
+          this.student = {}
+          alert('Cadastrado com sucesso!')
+          this.listar()
+          this.errors = {}
+        }).catch(e => {
+          this.errors = e.response.data.errors
+          console.log(e.response.data.errors)
+        })
+
+      }else{
+
+        Student.atualizar(this.student).then(() => {
+          this.student = {}
+          this.errors = {}
+          alert('Atualizado com sucesso!')
+          this.listar()
+        }).catch(e => {
+          this.errors = e.response.data.errors
+        })
+
+      }
+      
+
+    },
+
+    editar(student){
+      this.student = student
+    },
+
+    remover(ra){
+
+      if(confirm('Deseja excluir o produto?')){
+
+        Student.apagar(ra).then(() => {
+          this.listar()
+          this.errors = {}
+        }).catch(e => {
+          this.errors = e.response.data.errors
+        })
+
+      }
+
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
+
 </style>
